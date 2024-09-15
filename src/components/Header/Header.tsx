75fc7ceb61ce3ad3f088cccf9647395d258cc085
Beatriz Siquara <beatrizsiquara@gmail.com>
2024-09-15 16:21:25 -0300

    Feat [Criacao do componente de Modal]

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,21 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import logoImage from "../../assets/logo.svg";
+import * as Dialog from "@radix-ui/react-dialog";
+import { NewTransactionModal } from "../NewTransactionModal/NewTransactionModal";
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logoImage} alt="dt money" />
-        <NewTransactionButton type="button">Nova transação</NewTransactionButton>
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton type="button">
+              Nova transação
+            </NewTransactionButton>
+          </Dialog.Trigger>
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
