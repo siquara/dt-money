2f2658b58973b7e07649eb68f807c7e85f96a78f
Beatriz Siquara <beatrizsiquara@gmail.com>
2024-09-14 22:20:30 -0300

    Feat [ Configuracoes do projeto]

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/deafault";
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
-  return <h1>Hellowww</h1>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <h1>Hello, World!</h1>
+    </ThemeProvider>
+      
+  );
 }
