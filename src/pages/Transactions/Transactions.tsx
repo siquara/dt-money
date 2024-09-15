8c83d52a61500d9a72ec0d079047199bcf6c5f5a
Beatriz Siquara <beatrizsiquara@gmail.com>
2024-09-15 15:08:05 -0300

    Feat [Criacao do componente Search]

--- src/pages/Transactions/Transactions.tsx
+++ src/pages/Transactions/Transactions.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../../components/Header/Header";
 import { Summary } from "../../components/Summary/Summary";
+import { SearchForm } from "./components/SearchForm/SearchForm";
 import {
   PriceHighlight,
   TransactionsContainer,
@@ -12,6 +13,7 @@ export function Transactions() {
       <Header />
       <Summary />
       <TransactionsContainer>
+        <SearchForm />
         <TransactionsTable>
           <tbody>
             <tr>
